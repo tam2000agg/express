e57a1f386df6252bfa0ee2ae66b2d864283485a0
tam2000agg <tamannaggarwalmaur@gmail.com>
2021-02-17 13:13:13 +0530

    third commit

--- node_express/index.js
+++ node_express/index.js
@@ -2,11 +2,12 @@ const express=require('express');
 const http=require('http');
 const hostname='localhost';
 const port=3000;
-
+const morgan=require('morgan');
 const app=express();
-
+app.use(morgan('dev')); //dev=development and morgan is used to print the additional info. to screen
+app.use(express.static(__dirname+'/public'));
 app.use((req,res,next)=>{
-   console.log(req.headers);
+  // console.log(req.headers); u dont need this if u have morgan
    res.statusCode=200;
    res.setHeader('Content-Type','text/html');
    res.end('<html><body><h1>this is an express server</h1></body></html>') ;
